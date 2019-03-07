9a47d55e5c7331bd568090362671787b5a0d86d7
wright0776 <matwright2010@gmail.com>
2019-03-06 22:01:06 -0700

    fixed styling issues, esp for mobile

--- client/src/App/features/AboutMe/AboutMe.jsx
+++ client/src/App/features/AboutMe/AboutMe.jsx
@@ -3,12 +3,18 @@ import './aboutMe.css'
 
 function AboutMe() {
     return (
-        <div className='feature'>
-            <h2 className='title'>About Me</h2>
-            <p className='featureContent'>
-                I have been working with children who have special needs since 2012 as a Behavior Technician. I discovered my love for yoga and couldn’t get enough of the practice that was transforming my life. Wanting to explore and deepen my knowlege, I completed my 200 hour Power Yoga training. I have been teaching since 2017. I discovered Kids Yoga this year and it was an instant fit, bridging my two passions.
-            </p>
-            <div></div>
+        <div className='feature aboutMe'>
+
+            <div className='oliviaPortrait'></div>
+
+            <div className='wordsContainer'>
+
+                <h2 className='title'>About Me</h2>
+
+                <p className='aboutMeParagraph'>
+                    I have been working with children who have special needs since 2012 as a Behavior Technician. I discovered my love for yoga and couldn’t get enough of the practice that was transforming my life. Wanting to explore and deepen my knowlege, I completed my 200 hour Power Yoga training. I have been teaching since 2017. I discovered Kids Yoga this year and it was an instant fit, bridging my two passions.
+                </p>
+            </div>
         </div>
     )
 }
--- client/src/App/features/ClassSchedule/ClassSchedule.jsx
+++ client/src/App/features/ClassSchedule/ClassSchedule.jsx
@@ -3,11 +3,16 @@ import './classSchedule.css';
 
 function ClassSchedule() {
     return (
-        <div className='feature'>
+        <div className='feature classSchedule'>
+
             <h2 className='title'>Class Schedule</h2>
+
             <div className='featureContent'>
+
                 <h3>Family Yoga</h3>
+
                 <p>Every other Monday beginning March 25, 2019.</p>
+
                 <ul>
                     <li>March 25, 2019</li>
                     <li>April 8, 2019</li>
@@ -24,11 +29,11 @@ function ClassSchedule() {
                         <span>3474 S 2300 E #12</span>
                         <span>Salt Lake CIty, UT 84109</span>
                     </a>
+
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.994531720195!2d-111.82783698459555!3d40.69611807933363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87528aa11e827631%3A0xc95dc61a52d89337!2sVitalize+Community+%26+Healing+Arts+Studio!5e0!3m2!1sen!2sus!4v1551758520682"
-                        className='gmap' frameborder="0" allowfullScreen>
+                        className='locationMap' frameborder="0" allowfullScreen>
                     </iframe>
                 </div>
-
             </div>
         </div>
     )
